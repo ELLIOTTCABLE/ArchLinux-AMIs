a55b438d10a67f180d645f4337e1f972d3d90bea
elliottcable <git@elliottcable.name>
2009-08-12 23:36:10 -0800

    pam depends on db cracklib

--- instructions.bash
+++ instructions.bash
@@ -38,8 +38,8 @@ export EC2_AMITOOL_HOME="$(pwd)/ec2-ami-tools"
 # openssh - necessary to SSH in
 # curl - needed for the initscripts that download the pubkey
 # procps - ps, top, kill
-PACKS="bash gcc-libs db \
-  gdbm cracklib pam shadow attr acl gmp coreutils perl openssl tcp_wrappers \
+PACKS="bash gcc-libs \
+  gdbm pam shadow attr acl gmp coreutils perl openssl tcp_wrappers \
   e2fsprogs sqlite3 heimdal openssh run-parts findutils sed ca-certificates \
   curl binutils bzip2 cpio dash expat filesystem dbus-core dcron dhcpcd \
   dialog gzip texinfo diffutils file gawk gettext pcre grep groff \
