c8666889aa4818a1c2f0fb5662f209d41e9162fc
elliottcable <git@elliottcable.name>
2009-08-13 20:07:22 -0800

    Don't need bzip2

--- check_instance.bash
+++ check_instance.bash
@@ -15,7 +15,7 @@ ssh -o "StrictHostKeyChecking no" root@$INSTANCE_ADDRESS \
 
 # Install the packages we’ve removed
 pacman --noconfirm -S sudo wget which vi tar nano lzo2 procinfo libgcrypt \
-  less groff file diffutils dialog dbus-core dash cpio
+  less groff file diffutils dialog dbus-core dash cpio bzip2
 
 shutdown -h now && exit
 
--- instructions.bash
+++ instructions.bash
@@ -57,7 +57,7 @@ export EC2_AMITOOL_HOME="$(pwd)/ec2-ami-tools"
 PACKS=" \
   gmp \
   openssh \
-  curl binutils bzip2 filesystem dcron dhcpcd \
+  curl binutils filesystem dcron dhcpcd \
   gawk \
   util-linux-ng initscripts iputils \
   licenses logrotate \
