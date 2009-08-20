f99f4e83a6b52008765197f03854b9ee034aae09
elliottcable <git@elliottcable.name>
2009-08-20 00:02:54 -0800

    No longer checking instances in a seperate file, that's rolled in

--- bundle_ami.sh
+++ bundle_ami.sh
@@ -57,3 +57,47 @@ NAME=$(
 		ARI="$ARI"
 	SETUP
 )
+
+AMI=$(ec2-register --show-empty-fields "$BUCKET/$NAME.manifest.xml" \
+  | awk '/IMAGE/ { print $2 }')
+
+GROUPID=$(ec2-describe-group --show-empty-fields | awk '$1 == "GROUP" \
+  && $3 == "'$GROUP'" { print $3 }')
+if [[ -z $GROUPID ]]; then
+  ec2-add-group --show-empty-fields $GROUP \
+    -d "Instances dedicated to bundling AMIs" || exit 1
+  ec2-authorize --show-empty-fields $GROUP \
+    --protocol tcp --port-range 22 || exit 1
+  echo "-- Added security group: $GROUP"
+fi
+
+KEYID=$(ec2-describe-keypairs --show-empty-fields \
+  | awk '$1 == "KEYPAIR" && $2 == "'$KEY'" { print $2 }')
+if [[ -z $KEYID ]]; then
+  ec2-add-keypair --show-empty-fields $KEY \
+    > "id_rsa-$KEY" || exit 1
+  chmod 400 "id_rsa-$KEY" || exit 1
+  echo "-- Added keypair: $KEY"
+fi
+
+IID=$(ec2-run-instances --group $GROUP --key $KEY \
+  --availability-zone $AVAILABILITY_ZONE \
+  --instance-type $ITYPE $AMI | awk '/INSTANCE/ { print $2 }')
+IADDRESS="(nil)"
+while [[ $IADDRESS == "(nil)" ]]; do
+  IADDRESS=$(ec2-describe-instances --show-empty-fields $IID \
+    | awk '$1 == "INSTANCE" { print $4 }')
+done
+
+false
+until [[ $? == 0 ]]; do
+  sleep 5
+	ssh -o "StrictHostKeyChecking no" -i "id_rsa-$KEY" root@$IADDRESS <<-ITESTING
+		pacman --noconfirm -S sudo wget which vi tar nano lzo2 procinfo \
+		  libgcrypt less groff file diffutils dialog dbus-core dash cpio binutils
+		
+		shutdown -h now && exit
+	ITESTING
+done
+
+echo "** ${NAME} registered: ${AMI}"
