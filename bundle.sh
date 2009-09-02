3999725fa2b0530cc2be127557eb8add2cb23395
elliottcable <git@elliottcable.name>
2009-09-02 17:23:35 -0400

    Wrapping old bundling code in a function

--- bundle.sh
+++ bundle.sh
@@ -32,79 +32,81 @@ else
   ARI="ari-a51cf9cc"
 fi
 
-HOST_IID=$(./ami_bundler.sh get $HOST_ARCH) || exit 1
-
-HOST_IADDRESS="(nil)"
-while [[ $HOST_IADDRESS == "(nil)" ]]; do
-  HOST_IADDRESS=$(ec2-describe-instances --show-empty-fields $HOST_IID \
-    | awk '$1 == "INSTANCE" { print $4 }')
-done
-
-NAME=$(
-	cat "-" "./$2/bundle.sh" <<-SETUP | ssh -o "StrictHostKeyChecking no" -i "id_rsa-$HOST_KEY" root@$HOST_IADDRESS | tail -n1
-		source /root/.profile
+bundle() {
+  HOST_IID=$($0 host get $HOST_ARCH) || exit 1
+  
+  HOST_IADDRESS="(nil)"
+  while [[ $HOST_IADDRESS == "(nil)" ]]; do
+    HOST_IADDRESS=$(ec2-describe-instances --show-empty-fields $HOST_IID \
+      | awk '$1 == "INSTANCE" { print $4 }')
+  done
+  
+  NAME=$(
+		cat "-" "./$2/bundle.sh" <<-SETUP | ssh -o "StrictHostKeyChecking no" -i "id_rsa-$HOST_KEY" root@$HOST_IADDRESS | tail -n1
+			source /root/.profile
 		
-		AVAILABILITY_ZONE="$AVAILABILITY_ZONE"
-		KEY="$KEY"
-		GROUP="$GROUP"
-		BUCKET="$BUCKET"
-		HOST_ARCH="$HOST_ARCH"
-		HOST_EC2_ARCH="$HOST_EC2_ARCH"
-		HOST_AMI="$HOST_AMI"
-		HOST_ITYPE="$HOST_ITYPE"
-		ARCH="$ARCH"
-		EC2_ARCH="$EC2_ARCH"
-		ITYPE="$ITYPE"
-		AKI="$AKI"
-		ARI="$ARI"
-	SETUP
-)
-
-AMI=$(ec2-register --show-empty-fields "$BUCKET/$NAME.manifest.xml" \
-  | awk '/IMAGE/ { print $2 }')
-
-GROUPID=$(ec2-describe-group --show-empty-fields | awk '$1 == "GROUP" \
-  && $3 == "'$GROUP'" { print $3 }')
-if [[ -z $GROUPID ]]; then
-  ec2-add-group --show-empty-fields $GROUP \
-    -d "Instances dedicated to bundling AMIs" || exit 1
-  ec2-authorize --show-empty-fields $GROUP \
-    --protocol tcp --port-range 22 || exit 1
-  echo "-- Added security group: $GROUP"
-fi
-
-KEYID=$(ec2-describe-keypairs --show-empty-fields \
-  | awk '$1 == "KEYPAIR" && $2 == "'$KEY'" { print $2 }')
-if [[ -z $KEYID || ! -f "id_rsa-$KEY" ]]; then
-  ec2-delete-keypair --show-empty-fields $KEY
-  rm -f "id_rsa-$KEY"
-  ec2-add-keypair --show-empty-fields $KEY \
-    > "id_rsa-$KEY" || exit 1
-  chmod 400 "id_rsa-$KEY" || exit 1
-  echo "-- Added keypair: $KEY"
-fi
-
-IID=$(ec2-run-instances --group $GROUP --key $KEY \
-  --availability-zone $AVAILABILITY_ZONE \
-  --instance-type $ITYPE $AMI | awk '/INSTANCE/ { print $2 }')
-IADDRESS="(nil)"
-while [[ $IADDRESS == "(nil)" ]]; do
-  IADDRESS=$(ec2-describe-instances --show-empty-fields $IID \
-    | awk '$1 == "INSTANCE" { print $4 }')
-done
-
-false
-until [[ $? == 0 ]]; do
-  sleep 5
-	ssh -o "StrictHostKeyChecking no" -i "id_rsa-$KEY" root@$IADDRESS <<-ITESTING
-		pacman --noconfirm -S sudo wget which vi tar nano lzo2 procinfo \
-		  libgcrypt less groff file diffutils dialog dbus-core dash cpio binutils
+			AVAILABILITY_ZONE="$AVAILABILITY_ZONE"
+			KEY="$KEY"
+			GROUP="$GROUP"
+			BUCKET="$BUCKET"
+			HOST_ARCH="$HOST_ARCH"
+			HOST_EC2_ARCH="$HOST_EC2_ARCH"
+			HOST_AMI="$HOST_AMI"
+			HOST_ITYPE="$HOST_ITYPE"
+			ARCH="$ARCH"
+			EC2_ARCH="$EC2_ARCH"
+			ITYPE="$ITYPE"
+			AKI="$AKI"
+			ARI="$ARI"
+		SETUP
+  )
+  
+  AMI=$(ec2-register --show-empty-fields "$BUCKET/$NAME.manifest.xml" \
+    | awk '/IMAGE/ { print $2 }')
+  
+  GROUPID=$(ec2-describe-group --show-empty-fields | awk '$1 == "GROUP" \
+    && $3 == "'$GROUP'" { print $3 }')
+  if [[ -z $GROUPID ]]; then
+    ec2-add-group --show-empty-fields $GROUP \
+      -d "Instances dedicated to bundling AMIs" || exit 1
+    ec2-authorize --show-empty-fields $GROUP \
+      --protocol tcp --port-range 22 || exit 1
+    echo "-- Added security group: $GROUP"
+  fi
+  
+  KEYID=$(ec2-describe-keypairs --show-empty-fields \
+    | awk '$1 == "KEYPAIR" && $2 == "'$KEY'" { print $2 }')
+  if [[ -z $KEYID || ! -f "id_rsa-$KEY" ]]; then
+    ec2-delete-keypair --show-empty-fields $KEY
+    rm -f "id_rsa-$KEY"
+    ec2-add-keypair --show-empty-fields $KEY \
+      > "id_rsa-$KEY" || exit 1
+    chmod 400 "id_rsa-$KEY" || exit 1
+    echo "-- Added keypair: $KEY"
+  fi
+  
+  IID=$(ec2-run-instances --group $GROUP --key $KEY \
+    --availability-zone $AVAILABILITY_ZONE \
+    --instance-type $ITYPE $AMI | awk '/INSTANCE/ { print $2 }')
+  IADDRESS="(nil)"
+  while [[ $IADDRESS == "(nil)" ]]; do
+    IADDRESS=$(ec2-describe-instances --show-empty-fields $IID \
+      | awk '$1 == "INSTANCE" { print $4 }')
+  done
+  
+  false
+  until [[ $? == 0 ]]; do
+    sleep 5
+		ssh -o "StrictHostKeyChecking no" -i "id_rsa-$KEY" root@$IADDRESS <<-ITESTING
+			pacman --noconfirm -S sudo wget which vi tar nano lzo2 procinfo \
+			  libgcrypt less groff file diffutils dialog dbus-core dash cpio binutils
 		
-		shutdown -h now && exit
-	ITESTING
-done
-
-echo "** ${NAME} registered: ${AMI}"
+			shutdown -h now && exit
+		ITESTING
+  done
+  
+  echo "** ${NAME} registered: ${AMI}"
+}
 
 start_host() {
   HOST_GROUPID=$(ec2-describe-group --show-empty-fields | awk '$1 == "GROUP" \
