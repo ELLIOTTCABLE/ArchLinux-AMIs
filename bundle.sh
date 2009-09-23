610698e275aefc4f6c079686722ac4db576ce7c8
elliottcable <git@elliottcable.name>
2009-09-22 16:12:59 -0800

    Arch updated Ruby to 1.9.1, which isn’t compatible with the EC2 AMI tools

--- bundle.sh
+++ bundle.sh
@@ -174,7 +174,9 @@ start_host() {
 		pacman --noconfirm -Syu
 		pacman --noconfirm -Syu
 		
-		pacman --noconfirm -S ruby unzip rsync lzma cpio
+		pacman --noconfirm -S unzip rsync lzma cpio
+		
+		pacman --noconfirm -U http://arm.kh.nu/extra::2009-9-13/os/i686/ruby-1.8.7_p174-1-i686.pkg.tar.gz
 		
 		pacman --noconfirm -Sc
 		
