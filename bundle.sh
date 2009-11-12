cdf560f6a29e897b9e3b71539e46769070afbb92
elliottcable <me@ell.io>
2009-11-12 04:33:35 -0900

    That Unicode was getting annoying. Doesn’t really look like an emdash in the Terminal, sadly.

--- bundle.sh
+++ bundle.sh
@@ -67,7 +67,7 @@ _usage() {
 		    EC2_CERT: Absolute path to your EC2 X.509 certificate
 		    
 		    AWS_ACCOUNT_NUMBER: Your numerical AWS account number (i.e.
-		      3161-7741-1691—will be read from $EC2_HOME/account_number
+		      3161-7741-1691, will be read from $EC2_HOME/account_number
 		      if undefined)
 		    S3_ACCESS_KEY: Your S3 access key (will be read from
 		      $EC2_HOME/access_key if undefined)
